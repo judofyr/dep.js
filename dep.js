610426e63a72ac4e04cfdfbb9c548d95283840e4
Magnus Holm <judofyr@gmail.com>
2013-01-19 21:11:01 +0100

    Refactor

--- dep.js
+++ dep.js
@@ -1,19 +1,4 @@
 (dep = function(ctx){
-  // Inverse graph between dependencies.
-  // If "app" depends on "a" and "b":
-  //   graph["a"] == ["app"]
-  //   graph["b"] == ["app"]
-  var graph = {};
-
-  // Number of pending dependencies.
-  // If "app" depends on two other modules:
-  //   count["app"] == 2
-  var count = {};
-
-  // List of dependencies per module
-  //    dependencies["app"] = ["a", "b"]
-  var dependencies = {};
-
   // Already loaded modules.
   var loaded = {};
 
@@ -29,77 +14,51 @@
   var uses = {};
 
   // Attempt to the load a module.
-  function load(name, cb) {
-    var deps, dep
-      // Setup the the `uses`-array
-      , use = (uses[name] || (uses[name] = []))
-
-    if (cb) use.push(cb);
-
-    if (deps = dependencies[name]) {
-      // This module has already been defined somewhere elase.
-
-      dependencies[name] = 0;
-
-      // If there's no dependencies, we can set it up right away.
-      if (!deps.length) setup(name);
-
-      // Otherwise, recursively try to load its dependencies.
-      while (dep = deps.shift()) load(dep);
-
+  function load(name) {
+    var fact = factory[name];
+    if (fact) {
+      // Only invoke the factory once
+      factory[name] = 0;
+      fact();
     } else if (ctx.load) {
       ctx.load(name);
     }
   }
 
-  // This is called whenever we want to setup a module.
-  function setup(name) {
-    var children = graph[name]
-      , use = uses[name]
-      , x
+  ctx.use = function(deps, cb) {
+    if (typeof deps == "string") deps = [deps];
 
-    if (!loaded[name]) {
-      loaded[name] = 1;
-      // Invoke factory (if any)
-      if (x = factory[name]) x();
-      // Invoke any users
-      while (x = use.shift()) x()
-
-      // Iterate over the graph and figure out if we can now invoke any
-      // other modules.
-      while (x = children && children.shift()) {
-        if (--count[x] == 0) {
-          // This was the final dependency for this child.
-          setup(x);
-        }
+    var left = deps.length+1, dep
+      , done = function() {
+        if (--left == 0) cb && cb();
       }
-    }
-  }
 
-  ctx.use = function(dep, cb) {
-    if (loaded[dep]) {
-      cb();
-    } else {
-      load(dep, cb);
+    // Make sure we handle when deps is empty
+    done();
+
+    while (dep = deps.shift()) {
+      if (loaded[dep]) {
+        done();
+      } else {
+        (uses[dep] || (uses[dep] = [])).push(done);
+        load(dep);
+      }
     }
   }
 
   ctx.define = function(name, deps, cb) {
-    var i, dep, currentDeps = [];
+    factory[name] = function() {
+      ctx.use(deps || [], function() {
+        var f, use = uses[name];
+        loaded[name] = 1;
+        if (cb) cb();
+        while (f = use && use.shift()) f();
+      });
+    };
 
-    while (dep = deps && deps.shift()) {
-      // We don't care about dependencies that has already been loaded
-      if (!loaded[dep]) {
-        currentDeps.push(dep);
-        (graph[dep] || (graph[dep] = [])).push(name);
-      }
+    if (uses[name]) {
+      load(name);
     }
-
-    dependencies[name] = currentDeps;
-    count[name] = currentDeps.length;
-    factory[name] = cb;
-
-    if (uses[name]) load(name);
   };
 
   return ctx;
